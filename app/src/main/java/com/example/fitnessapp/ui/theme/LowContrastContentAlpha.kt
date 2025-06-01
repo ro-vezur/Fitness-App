56d12dab22a00dcf95b2eb9bdf716a3c4ecccaa9
ro-vezur <authenttesting@gmail.com>
2025-06-01 18:25:08 +0300

    deleted/renamed files

--- app/src/main/java/com/example/fitnessapp/ui/theme/LowContrastContentAlpha.kt
+++
@@ -1,7 +0,0 @@
-package com.example.fitnessapp.ui.theme
-
-private object LowContrastContentAlpha {
-    const val high: Float = 0.87f
-    const val medium: Float = 0.60f
-    const val disabled: Float = 0.38f
-}
