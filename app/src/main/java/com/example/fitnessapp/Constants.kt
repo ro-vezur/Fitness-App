3f09f2a73a5cdb8f7f9a6b5d2f246108a67df915
ro-vezur <authenttesting@gmail.com>
2025-06-01 18:22:19 +0300

    constants

--- app/src/main/java/com/example/fitnessapp/Constants.kt
+++ app/src/main/java/com/example/fitnessapp/Constants.kt
@@ -1,3 +1,4 @@
 package com.example.fitnessapp
 
 const val clickDebounce = 1L
+const val CODE_LENGTH = 4
--- app/src/main/java/com/example/fitnessapp/ui/theme/UiConstants.kt
+++ app/src/main/java/com/example/fitnessapp/ui/theme/UiConstants.kt
@@ -1,7 +1,5 @@
 package com.example.fitnessapp.ui.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.unit.dp
 
-val BASE_BUTTON_HEIGHT = 60.dp
-val BASE_BUTTON_SHAPE = RoundedCornerShape(16.dp)
+
+const val BASE_SCREEN_WIDTH_RATIO = 0.85f
